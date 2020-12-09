99e1f72f14ebdbb9d419526cea65e65cb4d08f51
Sckripi <72759883+Sckripi@users.noreply.github.com>
2020-12-09 13:20:10 +0200

    Add files via upload

---
+++ MyWeatherApp/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace MyWeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lHumidity = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lTemp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Город";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Енакиево",
+            "Донецк",
+            "Снежное",
+            "Горловка"});
+            this.comboBox1.Location = new System.Drawing.Point(95, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(233, 38);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(538, 82);
+            this.panel1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Image = global::MyWeatherApp.Properties.Resources._4NEASq_TZ5w__2_;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(347, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Обновить";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 30);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Город";
+            // 
+            // lHumidity
+            // 
+            this.lHumidity.AutoSize = true;
+            this.lHumidity.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lHumidity.Location = new System.Drawing.Point(189, 155);
+            this.lHumidity.Name = "lHumidity";
+            this.lHumidity.Size = new System.Drawing.Size(65, 30);
+            this.lHumidity.TabIndex = 6;
+            this.lHumidity.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Влажность";
+            // 
+            // lTemp
+            // 
+            this.lTemp.AutoSize = true;
+            this.lTemp.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTemp.Location = new System.Drawing.Point(189, 105);
+            this.lTemp.Name = "lTemp";
+            this.lTemp.Size = new System.Drawing.Size(65, 30);
+            this.lTemp.TabIndex = 4;
+            this.lTemp.Text = "label4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Температура";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MyWeatherApp.Properties.Resources.weather_seamless_pattern_36298_343;
+            this.ClientSize = new System.Drawing.Size(538, 237);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lTemp);
+            this.Controls.Add(this.lHumidity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lHumidity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lTemp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MyWeatherApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MyWeatherApp
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string citi = "Yenakiyeve";
+        public void info()
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={citi}&appid=b84317f5648a45ffd98987aa8452d409&units=metric";
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string response = reader.ReadToEnd();
+            weatherResponse wr = JsonConvert.DeserializeObject<weatherResponse>(response);
+            lTemp.Text = wr.main.Temp.ToString();
+            lHumidity.Text = wr.main.humidity.ToString() + " %";
+            double temp = Convert.ToDouble(lTemp.Text);
+
+            if (temp > 10 && temp < 20)
+            {
+                lTemp.Text += ". Достаточно тепло!";
+            }
+            else if (temp < 10 && temp > 0)
+            {
+                lTemp.Text += ". Прохладно!";
+            }
+            else if (temp < 0 && temp > -10)
+            {
+                lTemp.Text += ". На улице дубак!";
+            }
+            else if ( temp < -10)
+            {
+                lTemp.Text += ". Сиди лучше дома...";
+            }
+            else if (temp > 20)
+            {
+                lTemp.Text += ". Адская жара";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBox1.SelectedIndex = 0;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            info();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            switch (comboBox1.SelectedIndex) {
+                case 0:
+                    citi = "Yenakiyeve";
+                    break;
+                case 1:
+                    citi = "Donetsk";
+                    break;
+                case 2:
+                    citi = "Snizhne";
+                    break;
+                case 3:
+                    citi = "Horlivka";
+                    break;
+            }
+            info();
+
+        }
+
+        
+
+    }
+}
